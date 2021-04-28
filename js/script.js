5ef14efb9397b18b0c1cce9e0345617cfff8cec8
Anastasiya <example@gmail.com>
2021-04-28 13:35:39 +0500

    Первая практика

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const answers = [];
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
-document.write(answers);
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+
+      console.log(personalMovieDB);
